3347f82419cf5ba549c09b5e71230109beb61fcf
Amy Egan <amyegan@paypal.com>
2021-05-25 16:12:36 -0400

    Add createOrder and onApprove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-integration';
+  price = 0.03;
 }
--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,20 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { loadScript } from '@paypal/paypal-js';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.css']
+  styleUrls: ['./checkout.component.css'],
 })
 export class CheckoutComponent implements OnInit {
-  CLIENT_ID = 'your-client-id';
+  CLIENT_ID = 'test';
+  @Input() cartTotal: Number = 0.02;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    loadScript({'client-id': this.CLIENT_ID})
-      .then(paypal => {
-        paypal.Buttons().render('#paypal-button-container');
-      });
+    loadScript({ 'client-id': this.CLIENT_ID }).then((paypal) => {
+      paypal
+        .Buttons({
+          createOrder: this.createOrder,
+          onApprove: this.onApprove,
+        })
+        .render('#paypal-button-container');
+    });
+  }
+
+  createOrder = (data: any, actions: any) => {
+    console.log('Create order...');
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: this.cartTotal,
+          },
+        },
+      ],
+    });
+  }
+
+  onApprove = (data: any, actions: any) => {
+    console.log('Order approved...');
+    return actions.order.capture().then(alert('Order complete!'));
   }
 }
